db3bc167dbb8f9a91c29e003e4abce594e5d656b
unknown <yeungson@gmail.com>
2025-08-25 20:50:56 +0100

    Fixed naming error in calendar.tsx

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -51,9 +51,10 @@ function Calendar({
                 day_hidden: "invisible",
                 ...classNames,
             }}
+
             components={{
-                iconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-                iconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+                IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+                IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
             }}
             {...props}
         />
