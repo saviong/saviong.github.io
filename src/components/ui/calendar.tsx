b9be62b26486149a3539babcc074cc56ef3a6161
unknown <yeungson@gmail.com>
2025-08-25 21:28:33 +0100

    Fixed date picker error

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import * as React from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
@@ -33,7 +34,7 @@ function Calendar({
                 table: "w-full border-collapse space-y-1",
                 head_row: "flex",
                 head_cell:
-                    "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem] flex-1",
+                    "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
                 row: "flex w-full mt-2",
                 cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
                 day: cn(
@@ -49,6 +50,11 @@ function Calendar({
                     "aria-selected:bg-accent aria-selected:text-accent-foreground",
                 day_hidden: "invisible",
                 ...classNames,
+            }}
+
+            components={{
+                iconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+                iconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
             }}
             {...props}
         />
